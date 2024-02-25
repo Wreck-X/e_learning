798005c6fe182bb5ae90f1f7480804b503bc5d7c
Aaryanajith <aaryanajith45@gmail.com>
2024-02-25 17:49:37 +0530

    add: Login page

--- lib/views/pre_auth/el_sign_in.dart
+++ lib/views/pre_auth/el_sign_in.dart
@@ -2,22 +2,77 @@ import 'package:e_learning_frontend/routes/route_names.dart';
 import 'package:flutter/material.dart';
 
 class SignInView extends StatelessWidget {
-  const SignInView({super.key});
+  SignInView({super.key});
+
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, RouteNames.pageView);
-                },
-                child: Text("Login"))
-          ],
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                width: double.infinity,
+                height: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/e_learning.png',
+                    scale: 3,
+                  )
+                ],
+              ),
+              const SizedBox(
+                width: double.infinity,
+                height: 90,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Login',
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              const SizedBox(
+                width: double.infinity,
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 336,
+                    height: 60,
+                    child: TextFormField(
+                      controller: _userNameController,
+                      decoration: const InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                          ),
+                        ),
+                        hintText: 'Username',
+                        hintStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
